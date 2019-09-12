d8bd2be26a93580e47eca3eaa907780feba7e061
cs15042 <cs15042@s.inf.shizuoka.ac.jp>
2019-09-12 12:07:26 +0900

    v3

--- blockly/generators/javascript/kanji/fuee.js
+++
@@ -1,6 +0,0 @@
-Blockly.JavaScript['fuee'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
-  var code = "0,0,100,100";
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
